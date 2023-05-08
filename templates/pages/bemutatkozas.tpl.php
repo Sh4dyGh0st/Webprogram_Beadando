9032c810b4947bb4096e330466eeddeb01311145
Bagó Bence József <106177569+Sh4dyGh0st@users.noreply.github.com>
2023-05-08 20:44:52 +0200

    V1
    V1

---
+++ templates/pages/bemutatkozas.tpl.php
@@ -0,0 +1,7 @@
+<h3>Rövid bemutatkozás:</h2>
+<p>Teherautók nyomkövetésére létrejött szolgáltatás vagyunk.
+Az elmúlt pár évben lettünk piacvezetők magyarországon a nyomkövetésben!
+Próbálja ki, nem fogja megbánni.</p>
+<p>Anyacégünk: <strong>Scania Hungária Kft.</strong></p>
+<p>Honlapjukat ide kattintva tekintheti meg:<a href="https://www.scania.com/hu/hu/home.html">Holnap</a></p>
+
